c3295a1be9b3fa9b515c1fde0a79fa2aed3328cd
John Damilola Ogungbure <jogungbure@gmail.com>
2022-06-12 23:08:08 +0100

    added automatic migration configuration to db setup

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -2,12 +2,13 @@ import os
 from pathlib import Path
 
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, init as migration_init
+import flask_migrate
 from src.library.helpers.logger import logger
 
 db = SQLAlchemy()
-migrate = Migrate()
+migrate = flask_migrate.Migrate()
 migration_file_path = f'{Path(Path.cwd())}/src/db/migrations'
+dev_env = os.environ.get('APP_ENV')
 
 
 def setup_db(app) -> None:
@@ -15,5 +16,10 @@ def setup_db(app) -> None:
   migrate.init_app(app, db, directory="src/db/migrations")
 
   if os.path.exists(migration_file_path) is False:
-    migration_init(directory='src/db/migrations')
+    flask_migrate.init(directory='src/db/migrations')
     logger.info('Migrations initialized')
+
+  revisions = flask_migrate.show(directory='src/db/migrations', revision='head')
+
+  if dev_env == 'production' and revisions != None:
+    flask_migrate.upgrade(directory='src/db/migrations', revision='head', sql=False, tag=None)
