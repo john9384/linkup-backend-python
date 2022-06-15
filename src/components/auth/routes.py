66d8c7ab201d7e66265cd75dc22db79f22670c30
John Damilola Ogungbure <jogungbure@gmail.com>
2022-06-16 00:53:18 +0100

    init login

--- src/components/auth/routes.py
+++ src/components/auth/routes.py
@@ -2,7 +2,7 @@ import os
 from flask import Blueprint, request
 from src.library.helpers.response_builder import success_response
 from src.library.helpers.raise_exception import raise_exception
-from .services import signup_service
+from .services import signup_service, login_service
 
 API_PREFIX = os.environ.get('API_PREFIX')
 auth = Blueprint("auth", __name__, url_prefix=f'{API_PREFIX}/auth')
@@ -15,6 +15,18 @@ async def signup():
     res_data = await signup_service(form_data)
 
     return success_response('Signup Complete', res_data)
+  except Exception as error:
+    print(error)
+    raise_exception(error)
+
+
+@auth.post('/login')
+async def login():
+  try:
+    form_data = request.json
+    res_data = await login_service(form_data)
+
+    return success_response('Login Successful', res_data)
   except Exception as error:
     print(error)
     raise_exception(error)
--- src/components/auth/services.py
+++ src/components/auth/services.py
@@ -23,3 +23,20 @@ async def signup_service(form_data: dict):
       'message': 'Signup Complete',
       'data': {'email': new_user.email}
   }
+
+
+async def login_service(form_data: dict):
+  # Validate the form data
+  # check if user with email exists
+  # fetch the user if the user exists
+  # compare the password with the one in db
+  # generate token to be use
+
+  ### TODO: Implement route protection ###
+  # TODO: complete login service
+  # TODO: complete token generation
+  # TODO: Custum form for validation
+  return {
+      'message': 'Login Complete',
+      'data': {'email': 'test@test.com', 'token': 'test'}
+  }
